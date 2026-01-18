e3fa61b618c14551fcb30ba5d9c5641e2e11725d
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-01-18 13:56:26 -0800

    adding 11-20 items

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -15,6 +15,18 @@
             Console.WriteLine("8. I have 3 speeding tickets");
             Console.WriteLine("9. I have 4 dogs");
             Console.WriteLine("10. I have 2 cats");
+            Console.Write("Press Enter for the next 11-20 things about me");
+            Console.ReadLine();
+            Console.WriteLine("11. ");
+            Console.WriteLine("12. ");
+            Console.WriteLine("13. ");
+            Console.WriteLine("14. ");
+            Console.WriteLine("15. ");
+            Console.WriteLine("16. ");
+            Console.WriteLine("17. ");
+            Console.WriteLine("18. ");
+            Console.WriteLine("19. ");
+            Console.WriteLine("20. "); 
             
 
         }
