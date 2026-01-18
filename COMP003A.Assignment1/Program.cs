ad6048bdbf996aa877fe341bc377cc61bd3a9ddc
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-01-18 13:43:53 -0800

    added 10 items

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -6,15 +6,16 @@
         {
             Console.WriteLine("100 Things About Me!");
             Console.WriteLine("1. My name is Isaac");
-            Console.WriteLine("2. ");
-            Console.WriteLine("3. ");
-            Console.WriteLine("4. ");
-            Console.WriteLine("5. ");
-            Console.WriteLine("6. ");
-            Console.WriteLine("7. ");
-            Console.WriteLine("8. ");
-            Console.WriteLine("9. ");
-            Console.WriteLine("10. ");
+            Console.WriteLine("2. I like going to the gym");
+            Console.WriteLine("3. I work at amazon");
+            Console.WriteLine("4. I run my department at work");
+            Console.WriteLine("5. I work overnights");
+            Console.WriteLine("6. my favorite color is black");
+            Console.WriteLine("7. I ride a motorcycle");
+            Console.WriteLine("8. I have 3 speeding tickets");
+            Console.WriteLine("9. I have 4 dogs");
+            Console.WriteLine("10. I have 2 cats");
+            
 
         }
     }
