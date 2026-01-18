56bf141b7eed10a749c17b4e20282ac216c6d237
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-01-18 13:36:59 -0800

    adding items 1-10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("100 Things About Me!");
+            Console.WriteLine("1. My name is Isaac");
+            Console.WriteLine("2. ");
+            Console.WriteLine("3. ");
+            Console.WriteLine("4. ");
+            Console.WriteLine("5. ");
+            Console.WriteLine("6. ");
+            Console.WriteLine("7. ");
+            Console.WriteLine("8. ");
+            Console.WriteLine("9. ");
+            Console.WriteLine("10. ");
+
         }
     }
 }
